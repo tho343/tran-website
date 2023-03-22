36fa3b2a9f31005a3f08ce6e1df2448b75271161
Tran Ho <tranho8596@gmail.com>
2023-03-22 12:45:39 -0700

    changed section style

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const navToggle = document.getElementById("nav-toggle");
 const links = document.getElementById("links");
 
-navToggle.addEventListener("click",(e)=>{
-    e.preventDefault();
+navToggle.addEventListener("click",function (){
+    
     links.classList.toggle("show-links");
 })
 links.classList.remove("show-links");
