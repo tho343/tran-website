c0ba4ea9d7564d6079013afab010564b91941073
Tran Ho <tranho8596@gmail.com>
2023-03-22 14:49:09 -0700

    added skills

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const navToggle = document.getElementById("nav-toggle");
 const links = document.getElementById("links");
+const aboutImage = document.getElementById("about-image");
+
+async function loadImage() {
+    console.log(aboutImage.attributes);
+}
 
 navToggle.addEventListener("click",function (){
     
     links.classList.toggle("show-links");
 })
 links.classList.remove("show-links");
+loadImage();
