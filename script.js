17c185b62aa93072a36b5b0cd216ca0049af5cda
Tran Ho <tranho8596@gmail.com>
2023-03-25 14:44:04 -0700

    added resume

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
+
 const navToggle = document.getElementById("nav-toggle");
 const links = document.getElementById("links");
 const aboutImage = document.getElementById("about-image");
 
-async function loadImage() {
-    console.log(aboutImage.attributes);
-}
+let  projects = [
+    {   id: 1,
+        name: "countdown app"
+    },
+    {   id: 2,
+        name: "money tracker app"
+    },{   id: 3,
+        name: "calculator"
+    },
 
+]
+console.log(projects);
 navToggle.addEventListener("click",function (){
     
     links.classList.toggle("show-links");
 })
 links.classList.remove("show-links");
-loadImage();
---
+++ utils.js
@@ -0,0 +1,11 @@
+export default  projects = [
+    {   id: 1,
+        name: "countdown app"
+    },
+    {   id: 2,
+        name: "money tracker app"
+    },{   id: 3,
+        name: "calculator"
+    },
+
+]
