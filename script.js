4dfe9d36c19e071c7951f5b75e3d849be928b135
Tran Ho <tranho8596@gmail.com>
2023-03-26 17:28:52 -0700

    styled form

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 const navToggle = document.getElementById("nav-toggle");
 const links = document.getElementById("links");
 const aboutImage = document.getElementById("about-image");
-
+const inputs = document.querySelectorAll('input');
+const labels = document.querySelectorAll("label");
 let  projects = [
     {   id: 1,
         name: "countdown app"
@@ -15,8 +16,21 @@ let  projects = [
 
 ]
 console.log(projects);
+//console.log(inputs);
 navToggle.addEventListener("click",function (){
     
     links.classList.toggle("show-links");
 })
+inputs.forEach((i)=>{
+    i.addEventListener("focus",(e)=>{
+        
+        e.target.parentNode.childNodes[1].classList.add('input-tap');
+    })
+    i.addEventListener("focusout",(e)=>{
+        
+        e.target.parentNode.childNodes[1].classList.remove('input-tap');
+    })
+    
+})
+
 links.classList.remove("show-links");
