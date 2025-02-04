91c877afd1710815af951845eb464c2d079172ba
Tran Ho <tranho8596@gmail.com>
2025-02-04 14:46:55 -0800

    updated

--- script.js
+++ script.js
@@ -1,39 +1,14 @@
+let toggleBtn = document.getElementById("toggle-menu");
+let menu = document.getElementsByClassName("menu")[0];
+let heroImg = document.getElementsByClassName("image-container")[0];
 
-const navToggle = document.getElementById("nav-toggle");
-const links = document.getElementById("links");
-const aboutImage = document.getElementById("about-image");
-const inputs = document.querySelectorAll('input');
-const labels = document.querySelectorAll("label");
-let  projects = [
-    {   id: 1,
-        name: "countdown app",
-        url: "https://tho343.github.io/custom-countdown/"
-    },
-    {   id: 2,
-        name: "money tracker app",
-        url: "https://tho343.github.io/react-money-tracker-app"
-    },{   id: 3,
-        name: "calculator",
-        url:""
-    },
-
-]
-console.log(projects);
-//console.log(inputs);
-navToggle.addEventListener("click",function (){
-    
-    links.classList.toggle("show-links");
+toggleBtn.addEventListener("click",(e)=>{
+    menu.classList.toggle("visible");
+    let close  = menu.classList.contains("visible");
+    if (close){
+        heroImg.style = "margin-top: 180px";
+    }
+    else{
+        heroImg.style = "margin-top: auto";
+    }
 })
-inputs.forEach((i)=>{
-    i.addEventListener("focus",(e)=>{
-        
-        e.target.parentNode.childNodes[1].classList.add('input-tap');
-    })
-    i.addEventListener("focusout",(e)=>{
-        
-        e.target.parentNode.childNodes[1].classList.remove('input-tap');
-    })
-    
-})
-
-links.classList.remove("show-links");
